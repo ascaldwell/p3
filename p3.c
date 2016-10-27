49caf82f00d3e727bb3a685fcbfd8084a251075b
ascaldwell <ascaldwell@albany.edu>
2016-10-27 10:49:09 -0400

    create linked list

---
+++ p3.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  p3
+//
+//  Created by Andrew Caldwell on 10/26/16.
+//  Copyright © 2016 Andrew Caldwell. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    typedef struct node         // define structure node -Andrew
+    {
+        int value;
+        struct node  *next;     // recursion for the pointer value -Andrew
+    } NODE;
+    
+    NODE *head;      // define and initialize the head of the linked list -Andrew
+    if ((head = (NODE*) malloc(sizeof(NODE)) == NULL))
+        printf("Could not allocate memory for head of list."); exit(1);
+        
+    
+    
+    return 0;
+}
