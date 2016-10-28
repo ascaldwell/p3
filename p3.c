e07c2a26f51715bbf40a99d0f18e7b73137586ed
ascaldwell <ascaldwell@albany.edu>
2016-10-28 14:20:28 -0400

    Create functional linked list

--- p3.c
+++ p3.c
@@ -7,20 +7,55 @@
 //
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-int main()
+
+#define MAX_CMD_LENGTH 100
+
+// void(print_list(NODE*);
+
+struct node
 {
-    typedef struct node         // define structure node -Andrew
-    {
-        int value;
-        struct node  *next;     // recursion for the pointer value -Andrew
-    } NODE;
-    
-    NODE *head;      // define and initialize the head of the linked list -Andrew
-    if ((head = (NODE*) malloc(sizeof(NODE)) == NULL))
-        printf("Could not allocate memory for head of list."); exit(1);
-        
-    
+    int val;
+    struct node *next;
+};
+
+struct node *head = NULL;
+struct node *curr = NULL;
+
+
+struct node *create_list(int val)
+{
+    printf("Creating list with head as [%d]", val);
+    struct node *ptr = (struct node*) malloc(sizeof(struct node));
+    if (ptr == NULL)
+        printf("Memeory could not be allocated for node creation. Exit with error.\n"); exit(1);
+
+
+    ptr->val = val;
+    ptr->next = NULL;
+
+    head = curr = ptr;
+    return ptr;
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
     
     return 0;
 }
+
+/* void print_list(NODE *head)
+{
+    NODE *current = head;
+    
+    while (current != NULL)
+    {
+        printf("%d\n", current->value);
+        current = current->next;
+    }
+}
+*/
