0aa8a00e3a618135c18e316fb38a828edfebde15
ascaldwell <ascaldwell@albany.edu>
2016-10-29 16:45:51 -0400

    Use template to create a linked list.

--- p3.c
+++ p3.c
@@ -9,44 +9,150 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-#define MAX_CMD_LENGTH 100
+#define MAX_CMD_LENGTH 4
 
 // void(print_list(NODE*);
 
-struct node
+typedef struct node
 {
-    int val;
+    int count;
+    char symbol;
+    //int *val = 0;
     struct node *next;
-};
+} Node;
 
-struct node *head = NULL;
-struct node *curr = NULL;
-
-
-struct node *create_list(int val)
+typedef struct ListTag
 {
-    printf("Creating list with head as [%d]", val);
-    struct node *ptr = (struct node*) malloc(sizeof(struct node));
-    if (ptr == NULL)
-        printf("Memory could not be allocated for node creation. Exit with error.\n"); exit(1);
+    struct NodeTag *first;
+} List;
+
+Node *head = NULL;
+Node *curr = NULL;
 
 
-    ptr->val = val;
-    ptr->next = NULL;
-
-    head = curr = ptr;
-    return ptr;
-
-}
+/* Prototypes for functions of a linked list */
+Node *create_list(char[]);      // prototype for create_list function
+Node *insert(int*, char*);
+Node *search(char*, struct node**);
+List *List_create();
+void List_append(List*, char*);
+Node *Node_create(void);
 
 
 int main(int argc, char *argv[])
 {
-
+    List *list = List_create();
+    char commandString[MAX_CMD_LENGTH];
+    int numValue;
+    char dataString[10];
+    printf("Command? ");
+    scanf("%s %s", commandString, dataString);
+    
+    if (isdigit(dataString[0]))
+        numValue = atoi(dataString);
+    else
+        create_list(dataString);
+    while (1)
+    {
+    if (strcmp(commandString, "ins") == 0)
+    {
+        List_append(list, dataString);
+        printf("[%s] has been added to the list!", dataString);
+        
+    }
+    else if (strcmp(commandString, "end") == 0)
+             break;
+    }
     
     return 0;
 }
+
+
+/* Creates a list if none exist. */
+
+List *List_create(void) {
+    List *list = malloc(sizeof(List));
+    if (list != NULL)
+        printf("Memory Allocation Failed."); exit(1);
+    
+    
+    Node *node = Node_create();
+    list->first = node;
+    
+    return list;
+    
+}
+
+
+/* Inserts a new node into the list the linked list. If the value already exists     */
+/* within the linked list the count will be incremented for the node with            */
+/* said value. The list is then sorted in descending order from highest count value. */
+
+
+void List_append(List *list, char *str)
+{
+    assert(list != NULL);
+    assert(str != NULL);
+        
+    Node *node = list->first;
+    while (node->next != NULL)
+    {
+        node = node->next;
+    }
+        
+    node->symbol = *str;
+    node->next = Node_create();
+}
+
+
+Node *Node_create(void) {
+    Node *node = malloc(sizeof(Node));
+    assert(node != NULL);
+    
+    node->data = "";
+    node->next = NULL;
+    
+    return node;
+}
+
+
+
+/* Search function to traverse the linked list to find a node with a specified */
+/* value and return it.                                                        */
+/*Node *search(char val[], Node **prev)
+{
+    struct node *ptr = head;
+    struct node *tmp = NULL;
+    int found = 0;
+    
+    printf("\nSearching for the list for value [%s] \n", val);
+    
+    while(ptr != NULL)
+    {
+        if (strcmp(&ptr->symbol, val) == 0)
+            {
+        found = 1;
+            break;
+        }
+        else
+        {
+            tmp = ptr;
+            ptr = ptr->next;
+        }
+    }
+    
+    if (found == 1)
+    {
+        if(prev)
+            *prev = tmp;
+        return ptr;
+    }
+    else
+        return NULL;
+}
+*/
 
 /* void print_list(NODE *head)
 {
